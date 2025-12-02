201ffc49d8ce2a1e329ab8f5aada28213f298786
harne_ <harne_@naver.com>
2025-12-02 19:18:13 +0900

    웹샘플 실행기 제거... 이 구성에선 안 될 것 같음

--- Jamaker/Jamaker.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start /D"C:\Program Files\Google\Chrome\Application\" chrome.exe ^
---disable-web-security ^
---user-data-dir=D:\Jamaker\browser ^
---disable-popup-blocking ^
---app=D:\workspace\VisualStudio\JamakerWV\Jamaker\Jamaker.html
